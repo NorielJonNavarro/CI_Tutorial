4e0546c7e70e8407147b1a14ae4d619cb63783aa
NorielJonNavarro <noriel@smedia.com.au>
2019-10-16 16:14:07 +0800

    Edited the index page and added some css

--- CodeIgniter-2.2.6/application/views/view_post.php
+++ CodeIgniter-2.2.6/application/views/view_post.php
@@ -6,13 +6,13 @@
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
-<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="<?php base_url()?>/css/style.css">
 <title>Title</title>
 </head>
 <body>
 <div class="view-content">
-            <h1 class="try">Chapters</h1>
-            
+           <div class="jumbotron view">
+           <h1 class="try">Chapters</h1>
             <?php
                 if (!isset($posts)) 
                 {
@@ -21,13 +21,16 @@
                 {
                     foreach($posts as $row) 
                     {
+                        echo "<div class='posts'>";
 			            echo "<h2><a href='".base_url()."posts/post/".$row['postID']."'>".$row['title']."</a></h2>";
 			            echo "<p>".strip_tags($row['post'], '0, 200')."...</p>";
-			            echo "<p><a href='".base_url()."posts/post/".$row['postID']."'>Read More</a></p>";
+                        echo "<p><a href='".base_url()."posts/post/".$row['postID']."'>Read More</a></p>";
+                        echo "</div>";
 		            }
 	            }
 	
             ?>
+           </div>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.slim.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.15.0/umd/popper.min.js"></script>
