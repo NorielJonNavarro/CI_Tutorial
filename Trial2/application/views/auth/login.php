e6757138391bf1c1821695a30e4f4506bae725da
NorielJonNavarro <noriel@smedia.com.au>
2019-10-18 14:30:36 +0800

    Front end update

--- Trial2/application/views/auth/login.php
+++ Trial2/application/views/auth/login.php
@@ -1,4 +1,22 @@
 <div class="d-flex flex-wrap justify-content-center align-content-center login-body">
     <div class="jumbotron login-form">
+    <?php echo form_open('auth/');?>
+        <div class="login-form-header">
+            <h1 class="text-center">User Login</h1>
+        </div>
+        <div class="login-form-body">
+            <div class="form-group">
+                <input id="username" class="form-control" type="text" name="username" placeholder="Username" required>
+            </div>
+
+            <div class="form-group">
+                <input id="password" class="form-control" type="text" name="password" placeholder="Password" required>
+            </div>
+            <div class="d-flex justify-content-center mt-4">
+                <button class="btn login-form-button" type="submit">Login</button>
+            </div>
+
+            <p class="text-center pt-3">Don't have a account?<a href="#"> Register</a></p>
+        </div>
     </div>
 </div>
