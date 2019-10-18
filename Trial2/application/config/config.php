94f50e667798185ccdfda96464e811af7d8cfb27
NorielJonNavarro <noriel@smedia.com.au>
2019-10-18 12:50:51 +0800

    Update

--- Trial2/application/config/config.php
+++ Trial2/application/config/config.php
@@ -232,7 +232,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = 'CsxLNX59IB8euklndwq0ky4pr3tYy87T';
 
 /*
 |--------------------------------------------------------------------------
